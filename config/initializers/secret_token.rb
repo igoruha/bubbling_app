01a47462ddc2fa48d16e4a6e50cb0403b8b80ea6
Igor V. Shishkanov <igor-shishkanov@yandex.ru>
2012-09-05 14:10:45 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BubblingApp::Application.config.secret_token = '6bd4290d6a06e0b6ce8deeeb0f98fcfb46ea655bf8ac5008a3ac744b9bbb53b89324fc185636763c2e619da7848526f82243ec8eaf7733d7e654e03ef8308613'
